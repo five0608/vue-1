929ef91e28284a6369f0169a252635644442eb87
five <five.950608@gmail.com>
2023-12-19 11:34:15 +0900

    평가1

--- js/app.js
+++ js/app.js
@@ -5,8 +5,6 @@ var cmp = {
 Vue.component('todo-footer', {
   template: '<p>This is another global child component</p>'
 });
-// 실습 #1 - `todo-footer` 컴포넌트 전역 등록
-// <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
 
 var app = new Vue({
   el: '#app',
@@ -17,6 +15,5 @@ var app = new Vue({
   components: {
     'todo-list': cmp
   }
-  // 실습 #2 - `todo-list` 컴포넌트 지역 등록
-  // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+  
 });
